3303d5b9d844e3272f8f9932bc1de672f9713ea3
Alejandro Aldea <alejandro.aldea@usach.cl>
2025-11-03 15:06:15 -0300

    Update README.md

--- LiquiVerde/src/main/java/com/example/LiquiVerde/config/WebConfig.java
+++ LiquiVerde/src/main/java/com/example/LiquiVerde/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.LiquiVerde.config; // Ajuste el paquete si es necesario
+package com.example.LiquiVerde.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
